96bed037a8c9a83e94664d661ddf4e8667e329f2
jfr <jfr@n-side.com>
2023-09-09 15:22:48 +0200

    Add circle moving

--- kernel.c
+++ kernel.c
@@ -1,10 +1,22 @@
 #include "io.h"
+#include "fb.h"
 
-void main(){
+void main()
+{
     uart_init();
-    uart_writeText("Hello, world! This is the Kernel Club\n");
+    fb_init();
 
-    while(1){
-        uart_update();
+    drawRect(0, 0, 1920, 1080, 0x23, 1);
+    drawCircle(960,540,50,0x13,1);
+
+    int radius = 50;
+    int parity = 1;
+
+    while (1) {
+        drawRect(0, 0, 1920, 1080, 0x23, 1);
+        drawCircle(960,540,radius,0x13,1);
+        radius += parity;
+        if(radius == 100 || radius == 50)
+            parity *= -1;
     }
 }
